a3c1ea1d4e17c353daa93435c4902555fae69c4f
490513975@qq.com <490513975@qq.com>
2021-11-05 14:44:04 +0800

    1

--- admin/src/api/user.js
+++ admin/src/api/user.js
@@ -1,9 +1,7 @@
 import service from '@/utils/request'
 
 // @Summary 用户登录
-// @Produce  application/json
-// @Param data body {username:"string",password:"string"}
-// @Router /base/login [post]
+// @Param body {username:"string",password:"string"}
 export const login = (data) => {
   return service({
     url: '/base/login',
