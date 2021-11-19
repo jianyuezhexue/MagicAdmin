c0787b6bad490122aad519b575755619f27de599
490513975@qq.com <490513975@qq.com>
2021-11-19 13:01:37 +0800

    1

--- server/router/router.go
+++ server/router/router.go
@@ -9,7 +9,8 @@ import (
 // Routers 初始化总路由
 func Routers() *gin.Engine {
 	var Router = gin.Default()
-	//获取路由组实例
+
+	// 公共路由组
 	PublicGroup := Router.Group("")
 	{
 		// 健康监测
@@ -18,11 +19,14 @@ func Routers() *gin.Engine {
 		})
 		// 系统-用户登录
 	}
+
+	// 私有路由组
 	PrivateGroup := Router.Group("")
 	PrivateGroup.Use(middleware.JWTAuth())
 	{
-
+		// 系统-查询所有菜单
 	}
+
 	magic.Logger.Info("router register success")
 	return Router
 }
