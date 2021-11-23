f8023850f8487c2746a9dd3e3423b1fefbeee888
490513975@qq.com <490513975@qq.com>
2021-11-23 18:12:07 +0800

    1

--- server/service/system/user.go
+++ server/service/system/user.go
@@ -22,7 +22,7 @@ func Register(data system.FormRegister) (res system.User, err error) {
 
 	// 查重
 	find := magic.Orm.Where("userName = ?", user.Username).First(user).Error
-	if !errors.Is(find, gorm.ErrRecordNotFound) { // 判断用户名是否注册
+	if !errors.Is(find, gorm.ErrRecordNotFound) {
 		return *user, errors.New("用户名已注册")
 	}
 
