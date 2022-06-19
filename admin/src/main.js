f413e7f35c70349973bedda0e891b6d5b1fcc869
490513975 <490513975.com>
2022-06-19 17:53:25 +0800

    1

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { store } from '@/store/index'
+import router from '@/router/index'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(ElementPlus, { locale: zhCn })
+    .mount('#app')
