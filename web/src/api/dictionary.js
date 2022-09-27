682b9142a688e550d81e4b80615bed1a0064b074
490513975 <490513975@qq.com>
2022-09-27 16:06:44 +0800

    整理接口文件

---
+++ web/src/api/dictionary.js
@@ -0,0 +1,46 @@
+import service from '@/utils/request'
+
+// 分页获取字典目录列表
+export const getDictionaryList = (params) => {
+  return service({
+    url: '/dictionary',
+    method: 'GET',
+    params
+  })
+}
+
+// 创建字典目录
+export const createDictionary = (data) => {
+  return service({
+    url: '/dictionary',
+    method: 'POST',
+    data
+  })
+}
+
+// 删除字典目录
+export const deleteDictionary = (data) => {
+  return service({
+    url: '/dictionary',
+    method: 'DELETE',
+    data
+  })
+}
+
+// 更新字典目录
+export const updateDictionary = (data) => {
+  return service({
+    url: '/dictionary',
+    method: 'PUT',
+    data
+  })
+}
+
+// 用id查询字典目录
+export const findDictionary = (id) => {
+  return service({
+    url: `/dictionary/${id}`,
+    method: 'GET',
+    params
+  })
+}
