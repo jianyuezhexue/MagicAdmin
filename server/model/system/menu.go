03b4d45faceac01bf37a507ab5e9d3a09e3bf0b7
490513975 <490513975@qq.com>
2022-09-27 15:44:53 +0800

    json

--- server/model/system/menu.go
+++ server/model/system/menu.go
@@ -25,9 +25,9 @@ type Api struct {
 
 // 拓展权限结构
 type ExtAuth struct {
-	Type string `json:"type"`  // 权限类型:按钮权限，数据权限
-	Name string `json:"name"`  // 权限中文名
-	Val  string `json:"value"` // 字段值
+	Type string `json:"type"` // 权限类型:按钮权限，数据权限
+	Name string `json:"name"` // 权限中文名
+	Val  string `json:"val"`  // 字段值
 }
 
 type Array[T Api | ExtAuth] []T
@@ -57,17 +57,15 @@ func (a Array[T]) Value() (driver.Value, error) {
 type Menu struct {
 	model.BaseOrm
 	Meta      `json:"meta"`
-	ParentId  uint   `json:"parentId"`  // 父菜单ID
-	Path      string `json:"path"`      // 路由path
-	Name      string `json:"name"`      // 路由name
-	Hidden    bool   `json:"hidden"`    // 是否在列表隐藏
-	Component string `json:"component"` // 对应前端文件路径
-	Sort      int    `json:"sort"`      // 排序标记
-	// Apis      Array[Api]     `josn:"apis"`      // 菜单下的路由
-	// ExtAuth   Array[ExtAuth] `josn:"extAuth"`   // 菜单下的拓展权限
-	Apis     string `josn:"apis"`    // 菜单下的路由
-	ExtAuth  string `josn:"extAuth"` // 菜单下的拓展权限
-	Children []Menu `json:"children" gorm:"-"`
+	ParentId  uint           `json:"parentId"`  // 父菜单ID
+	Path      string         `json:"path"`      // 路由path
+	Name      string         `json:"name"`      // 路由name
+	Hidden    bool           `json:"hidden"`    // 是否在列表隐藏
+	Component string         `json:"component"` // 对应前端文件路径
+	Sort      int            `json:"sort"`      // 排序标记
+	Apis      Array[Api]     `json:"apis"`      // 菜单下的路由
+	ExtAuth   Array[ExtAuth] `json:"extAuth"`   // 菜单下的拓展权限
+	Children  []Menu         `json:"children" gorm:"-"`
 }
 
 type FormMenu struct {
@@ -77,7 +75,7 @@ type FormMenu struct {
 	Hidden    bool           `json:"hidden" form:"hidden"`                          // 是否在列表隐藏
 	Component string         `json:"component" form:"component" binding:"required"` // 对应前端文件路径
 	Sort      int            `json:"sort" form:"sort" binding:"numeric"`            // 排序标记
-	Apis      Array[Api]     `josn:"apis" gorm:"apis"`                              // 菜单下的路由
-	ExtAuth   Array[ExtAuth] `josn:"extAuth" gorm:"extAuth"`                        // 菜单下的拓展权限
+	Apis      Array[Api]     `json:"apis" gorm:"apis"`                              // 菜单下的路由
+	ExtAuth   Array[ExtAuth] `json:"extAuth" gorm:"extAuth"`                        // 菜单下的拓展权限
 	Meta      Meta           `json:"meta" gorm:"embedded"`                          // 附加属性
 }
