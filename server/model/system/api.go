e9978acd774ad62d00a2b90d94367e5e731d13e0
490513975 <490513975.com>
2022-10-03 15:40:31 +0800

    API模型数据补全

--- server/model/system/api.go
+++ server/model/system/api.go
@@ -5,7 +5,9 @@ import "github.com/jianyuezhexue/MagicAdmin/model"
 // Api属性
 type Api struct {
 	model.BaseOrm
-	Type  string `json:"type"`  // 请求类型
-	Name  string `json:"name"`  // 中文名
-	Route string `json:"route"` // 路由地址
+	MenuId   string `json:"menuId"`   // 菜单ID
+	MenuName string `json:"menuName"` // 菜单名称
+	Type     string `json:"type"`     // 请求类型
+	Name     string `json:"name"`     // 中文名
+	Route    string `json:"route"`    // 路由地址
 }
